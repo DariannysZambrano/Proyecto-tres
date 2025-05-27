d960ba652a87d03829c01c4ad482769c8aa04829
cely <kevinlaverde3@gmail.com>
2025-05-27 11:23:50 -0500

    feat new project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    public class Main {
+        public static void main(String[] args) {
+            String nombre1="Mateo Andrés Pineda Beltrán";
+            String Nombre2="Mariana Marín Vanegas":
+                    String Nombre3="Edwin Smith Murillo Murillo";
+                            String Nombre4="Dariannys Daniela Zambrano Escobar";
+                                    String Nombre5="Geraldinne Jaramillo Avendaño";
+                                            String Nombre6="Dilan Esteban Villada mejia";
+                                                    String Nombre7="Justin Londoño Cárdenas";
+            int edad1=19, edad2=23, edad3=20, edad4=17, edad5=21 , edad6=22, edad7=18;
+            int cedula1=1066869266, cedula2=1000440959, cedula3=1111668224, cedula4=1127923098, cedula5=1000443480, cedula6=1193565987, cedula7=1020416721;
+
+            String barrio1 = "Belén Rincón",String barrio2 = "CASTILLA",String barrio3 = "Enciso",String barrio4 = "Cordoba",String barrio5 = "Maruchenga",String barrio6 = "Olivares",String barrio7 = "Bello";
+        }
     }
+}
 }
